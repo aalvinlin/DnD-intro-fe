2fa5a3f05983fa399657458a4657d9d908d450ac
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-03-29 00:40:27 -0700

    create login and signup forms

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
     <div className="content">
       <h1>Welcome to D&amp;D Intro!</h1>
         <div className="img-container">
-            <img src={logo}></img>
+            <img className="logo" src={logo}></img>
         </div>
       <h2>Learn the basics to start playing.</h2>
       <div className="login-signup-container">
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,47 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import logo from "../images/logo.png";
 
 const Login = () => {
 
-  return (
-    <>
-      <h1>Login</h1>
-    </>
-  );
+    const { register, handleSubmit, watch, errors } = useForm();
+
+    const onSubmit = data => {
+        
+        console.log(data);
+
+    }
+
+    return (
+        <div className="content">
+            <h1>D&amp;D Intro &ndash; Log In</h1>
+            
+            <div className="img-container">
+                <img className="logo logo-small" src={logo}></img>
+            </div>
+            
+            <div className="login-signup-form-container">
+
+                <form onSubmit={handleSubmit(onSubmit)}>
+
+                    <div>
+                        <label htmlFor="username">Username:</label>
+                        <input name="username" ref={register({ required: true })} />
+                        {errors.username && <p className="input-error">Username is required.</p>}
+                    </div>
+
+                    <div>
+                        <label htmlFor="password">Password:</label>
+                        <input name="password" type="password" ref={register({ required: true })} />
+                        {errors.password && <p className="input-error">Password is required.</p>}
+                    </div>
+
+                    <button className="large-button login">Log In</button>
+
+                </form>
+            </div>
+        </div>
+    );
 }
 
 export default Login;
