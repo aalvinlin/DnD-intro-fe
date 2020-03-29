8f9a7dc1de720b7f70dc65fc4b23e72191df87dc
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-03-28 21:44:55 -0700

    create Home component layout

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import logo from "../images/logo.png";
 
 const Home = () => {
 
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <div className="content">
+      <h1>Welcome to D&amp;D Intro!</h1>
+        <div className="img-container">
+            <img src={logo}></img>
+        </div>
+      <h2>Learn the basics to start playing.</h2>
+      <div className="login-signup-container">
+        <Link to="/signup">
+            <button className="large-button signup">Sign Up</button>
+        </Link>
+        <Link to="/login">
+            <button className="large-button login">Log In</button>
+        </Link>
+      </div>
+    </div>
   );
 }
 
