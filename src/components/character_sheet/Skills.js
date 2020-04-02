6dd7d89cef06600e6f5dee3ac5ef6e8fb15dd5d0
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-04-02 12:40:57 -0700

    display values from skill proficiencies

--- src/components/character_sheet/Skills.js
+++ src/components/character_sheet/Skills.js
@@ -1,5 +1,6 @@
 import React from "react";
 import SectionTitle from "./SectionTitle";
+import { getAbilityScoreBonus, formatBonus, abilityScoreFullNames } from "../../utils/utils";
 
 const Skills = ({character}) => {
 
@@ -8,8 +9,25 @@ const Skills = ({character}) => {
 
       <SectionTitle number="4" title="Skills" />
 
-      <div className="skills placeholder">
-        
+      <div className="skills">
+      {
+          Object.keys(character.skillProficiencies).map(ability => {
+
+            let isProficient = character.skillProficiencies[ability];
+            let savingThrowBonus = getAbilityScoreBonus(12);
+
+            if (isProficient)
+              { savingThrowBonus += character.proficiencyBonus; }
+
+            return (
+              <div className="proficiency_info">
+                <p className="saving_throw_bonus">{formatBonus(savingThrowBonus)}</p>
+                <div className={"proficiencyCircle " + (isProficient ? "proficient" : "")}></div>
+                <p className="saving_throw_name">{ability}</p>
+              </div>
+            )
+          })
+        }
       </div>
 
     </div>
