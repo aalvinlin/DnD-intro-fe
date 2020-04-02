c04e868ce6ca95d40840d44182170ba7c344f096
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-04-02 01:20:44 -0700

    fix hardcoded text in SectionTitle

--- src/components/character_sheet/SectionTitle.js
+++ src/components/character_sheet/SectionTitle.js
@@ -4,8 +4,8 @@ const SectionTitle = ({number, title}) => {
 
   return (
     <div className="section_title_container">
-        <div className="number">1</div>
-        <h2 className="section_title">Ability Scores</h2>
+        <div className="number">{number}</div>
+        <h2 className="section_title">{title}</h2>
     </div>
   );
 }
