6cdce9453f66ff0e4a628dd919282d6f6dd14997
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-04-02 12:21:36 -0700

    display saving throw information

--- src/components/character_sheet/SavingThrows.js
+++ src/components/character_sheet/SavingThrows.js
@@ -1,6 +1,8 @@
 import React from "react";
 import SectionTitle from "./SectionTitle";
 
+import { getAbilityScoreBonus, formatBonus, abilityScoreFullNames } from "../../utils/utils";
+
 const SavingThrows = ({character}) => {
 
   return (
@@ -9,7 +11,26 @@ const SavingThrows = ({character}) => {
       <SectionTitle number="3" title="Saving Throws" />
 
       <div className="saving_throws placeholder">
-        
+
+        {
+          Object.keys(character.savingThrowProficiencies).map(ability => {
+
+            let isProficient = character.savingThrowProficiencies[ability];
+            let savingThrowBonus = getAbilityScoreBonus(character.abilityScores[ability]);
+
+            if (isProficient)
+              { savingThrowBonus += character.proficiencyBonus; }
+
+            return (
+              <div className="proficiency_info">
+                <p className="bonus">{formatBonus(savingThrowBonus)}</p>
+                <div className={"proficiencyCircle " + (isProficient ? "proficient" : "")}></div>
+                <p>{abilityScoreFullNames[ability]}</p>
+              </div>
+            )
+          })
+        }
+
       </div>
 
     </div>
