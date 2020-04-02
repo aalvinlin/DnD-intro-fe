9c5356cc094354f15f0640952bb26f4b9aeb884d
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-04-02 01:20:17 -0700

    create SectionTitle component

--- src/components/character_sheet/AbilityScores.js
+++ src/components/character_sheet/AbilityScores.js
@@ -1,4 +1,5 @@
 import React from "react";
+import SectionTitle from "./SectionTitle";
 import { getBonusFormatted } from "../../utils/utils";
 
 const AbilityScores = ({character}) => {
@@ -15,10 +16,7 @@ const AbilityScores = ({character}) => {
   return (
     <div className="section_container">
       
-      <div className="section_title_container">
-          <div className="number">1</div>
-          <h2 className="section_title">Ability Scores</h2>
-      </div>
+      <SectionTitle number="1" title="Ability Scores" />
 
       <div className="ability_scores">
 
---
+++ src/components/character_sheet/SectionTitle.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SectionTitle = ({number, title}) => {
+
+  return (
+    <div className="section_title_container">
+        <div className="number">1</div>
+        <h2 className="section_title">Ability Scores</h2>
+    </div>
+  );
+}
+
+export default SectionTitle;
+
+
